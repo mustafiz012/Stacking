3cb30585a9f817234b71e00cca157778c601bd20
Md. Mustafizur Rahman <mustafiz012@outlook.com>
2017-07-09 19:01:55 +0600

    initialization

---
+++ app/src/main/java/com/example/musta/stacking/A44996203.java
@@ -0,0 +1,65 @@
+package com.example.musta.stacking;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+public class A44996203 extends AppCompatActivity implements TextWatcher {
+
+    EditText et1, et2, et3, et4, et5, et6, et7;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a44996203);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        et1 = (EditText) findViewById(R.id.editText1);
+        et2 = (EditText) findViewById(R.id.editText2);
+        et3 = (EditText) findViewById(R.id.editText3);
+        et4 = (EditText) findViewById(R.id.editText4);
+        et5 = (EditText) findViewById(R.id.editText5);
+        et6 = (EditText) findViewById(R.id.editText6);
+        et7 = (EditText) findViewById(R.id.editText7);
+        et1.addTextChangedListener(this);
+        et2.addTextChangedListener(this);
+        et3.addTextChangedListener(this);
+        et4.addTextChangedListener(this);
+        et5.addTextChangedListener(this);
+        et6.addTextChangedListener(this);
+        et7.addTextChangedListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    void watcher(){
+        
+    }
+}
